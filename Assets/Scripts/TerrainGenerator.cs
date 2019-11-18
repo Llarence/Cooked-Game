e91561ef5859e6533df1d968d5ae148a36857c65
Llarence <36804301+Llarence@users.noreply.github.com>
2019-11-17 18:18:08 -0800

    Added terrain fun

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public int size;
+    public int height;
+    public GameObject cube;
+
+    void Start(){
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Generate(){
+        for(int x = 0; x < size; x++){
+            for(int y = 0; y < height; y++){
+                for(int z = 0; z < size; z++){
+                    if(GenerationFunction((float)x, (float)y, (float)z, 0, 0, 0.3f, size, height, 3, 0.3f)){
+                         
+                    }
+                }
+            }
+        }
+    }
+
+    bool GenerationFunction(float x, float y, float z, int xOff, int zOff, float heightDet, int size, int height, int detail, float amount){
+        x += xOff;
+        z += zOff;
+
+        x = x / size * detail;
+        y = y / height * detail;
+        z = z / size * detail;
+
+        float AB = Mathf.PerlinNoise(x, y);
+        float AC = Mathf.PerlinNoise(x, z);
+        float BC = Mathf.PerlinNoise(y, z);
+
+        float BA = Mathf.PerlinNoise(y, x);
+        float CA = Mathf.PerlinNoise(z, x);
+        float CB = Mathf.PerlinNoise(z, y);
+
+        float ABC = AB + AC + BC + BA + CA + CB;
+        ABC /= 6;
+
+        if(ABC > amount + y * heightDet){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
