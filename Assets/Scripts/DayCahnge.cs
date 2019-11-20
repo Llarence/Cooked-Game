fc9f1eb140cf6bab98f28b8130f7afd4c71dcd65
Llarence <36804301+Llarence@users.noreply.github.com>
2019-11-19 21:20:42 -0800

    light change

---
+++ Assets/Scripts/DayCahnge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCahnge : MonoBehaviour
+{
+	void Update(){
+		//full day cycle 6 minutes
+		transform.Rotate(0, Time.deltaTime, 0);
+    }
+}
