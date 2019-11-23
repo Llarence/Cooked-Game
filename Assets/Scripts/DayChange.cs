c423c36d41adebca9349cc9ef6b245371a233e59
Llarence <36804301+Llarence@users.noreply.github.com>
2019-11-23 04:13:15 -0800

    fix time

--- Assets/Scripts/DayChange.cs
+++ Assets/Scripts/DayChange.cs
@@ -7,5 +7,11 @@ public class DayChange : MonoBehaviour
 	void Update(){
 		//full day cycle 6 minutes
 		transform.Rotate(Time.deltaTime, 0, 0);
+		//make sure lighting doesn't shine upsidedown
+		if(transform.eulerAngles.x >= 265 && transform.eulerAngles.x <= 355){
+			GetComponent<Light>().intensity = 0;
+		}else{
+			GetComponent<Light>().intensity = 1;
+		}
     }
 }
