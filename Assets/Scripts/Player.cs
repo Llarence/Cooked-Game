98cc6588ad56c2073490a80c2ef0ee90c357b8bc
Llarence <36804301+Llarence@users.noreply.github.com>
2019-11-28 03:44:20 -0800

    player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        transform.GetChild(0).Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        rb.velocity = transform.rotation * new Vector3(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y, Input.GetAxisRaw("Vertical") * speed);
+    }
+}
