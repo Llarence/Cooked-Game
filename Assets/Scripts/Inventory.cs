5169c1563d2fd326110074dac0b66a00e412d0cd
Gary18 <45544024+Gary18@users.noreply.github.com>
2020-01-05 11:39:22 -0800

    Finished Collecting

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,8 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+
+    public GameObject inventory;
+    public bool InventoryEnabled;
     public float mushrooms;
+    public Text Mushrooms;
+
+    void Update()
+    {
+        AccessInventory();
+
+        SetItemValues();
+    }
+
+    public void AccessInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && InventoryEnabled == false)
+        {
+            InventoryEnabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && InventoryEnabled == true)
+        {
+            InventoryEnabled = false;
+        }
+
+        if(InventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+
+    public void SetItemValues()
+    {
+        Mushrooms.text = "Mushrooms: " + mushrooms;
+    }
 }
--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -7,15 +7,30 @@ public class Plant : MonoBehaviour
 
     public bool clicked;
     public bool mushroom;
+    public float respawn;
+    public float respawnTime = 120f;
 
     void Update()
     {
-        if(clicked == true)
+        respawn += 1 * Time.deltaTime;
+        if (clicked == true)
         {
             if(mushroom == true)
             {
                 GameObject.Find("Player").GetComponent<Inventory>().mushrooms++;
-                Destroy(gameObject);
+                gameObject.GetComponent<MeshRenderer>().enabled = false;
+                gameObject.GetComponent<MeshCollider>().enabled = false;
+                respawn = 0f;
+                clicked = false;
+            }
+        }
+        else
+        {
+            if (respawn >= respawnTime)
+            {
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
+                gameObject.GetComponent<MeshCollider>().enabled = true;
+                respawn = 0f;
             }
         }
     }
